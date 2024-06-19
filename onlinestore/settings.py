3c039d5d625be8aa1459b6cb6090b74b728dda67
Mikosy <miibewuba@gmail.com>
2024-06-19 12:59:58 +0100

    added allowed host

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3j8v_l^k2ijlvfn*_nh^p15&fjd5dv^_z39^$o0vtzu=niub70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlinestore-app.onrender.com', 'localhost:5500']
 
 AUTH_USER_MODEL = 'core.CustomUser'
 
